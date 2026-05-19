4a76f393236798dbcbd6ef9faa5d777d7d0576c6
imzinfsec <imzinfsec@gmail.com>
2026-05-19 14:12:22 +0900

    gitops-app all

---
+++ ops-app2/src/main/java/ops/app2/OpsApp12Application.java
@@ -0,0 +1,13 @@
+package ops.app2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsApp12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpsApp12Application.class, args);
+	}
+
+}
---
+++ ops-app2/src/test/java/ops/app2/OpsApp12ApplicationTests.java
@@ -0,0 +1,13 @@
+package ops.app2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsApp12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
